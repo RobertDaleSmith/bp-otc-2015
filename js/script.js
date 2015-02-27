c16cd9b5d457db0d43f300966c328ab513528db7
Robert Dale Smith <rsmith@bouncingpixel.com>
2015-02-27 10:21:24 -0600

    Builds out dropDown menu.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-//Page and resources loaded.
-$(window).bind("load", function() {
-	var hello = $(".hello").text();
-	console.log(hello);
+
+$("#header_button_wrapper .btn .icon").click(function(event){
+
+	var self = this;
+
+	var isOpen = false;
+
+	if ($(self).parent().attr('class').indexOf("open") >= 0) isOpen = true;
+	
+	$("#header_button_wrapper .btn").removeClass('open');
+	if(!isOpen) $(self).parent().addClass('open');
+
+});
+
+
+$("#header_button_wrapper .btn .title").click(function(event){
+
+	var self = this;
+	
+	$("#header_button_wrapper .btn").removeClass('active');
+	$(self).parent().addClass('active');
+
+});
+
+$(".menuItem").click(function(event){
+
+	console.log( "isOpen" );
+	$("#header_button_wrapper .btn").removeClass('open');
+
 });
